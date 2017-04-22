c90fbe61f3631a5e78613ba2346db9d9c762f2f4
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-04-22 09:02:36 -0500

    Added mmc / partition info.

--- system.py
+++ system.py
@@ -1,4 +1,6 @@
 import re
+import os
+import os.path
 
 
 uptime_fields = ['uptime', 'idle']
@@ -39,8 +41,68 @@ def meminfo():
     return results
 
 
+def mediainfo():
+    blocks = [name for name in os.listdir('/sys/block') if name.startswith('mmcblk')]
+
+    for block in blocks:
+        path = os.path.join('/sys/block', block)
+
+        filename = os.join(path, 'device/size')
+        block_size = int(open(filename).readline()) * 512
+
+        filename = os.join(path, 'device/type')
+        block_media = open(filename).readline()
+
+        partitions = [name for name in os.listdir(path) if name.startswith(block)]
+
+        for partition in partitions:
+            pass
+
+
+def get_mmc_info(path):
+    mmc = {}
+
+    # units of size are 512 byte blocks
+    filename = os.join(path, 'size')
+    mmc['size'] = int(open(filename).readline()) * 512
+
+    # type is either SD or MMC
+    filename = os.join(path, 'device/type')
+    mmc['type'] = open(filename).readline().lower()
+
+    mmc['partitions'] = []
+
+    for name in [name for name in os.listdir(path) if name.startswith('mmc')]:
+        mmc['partitions'].append(get_partition_info(os.join(path, name)))
+
+    for part in mmc['partitions']:
+        part['ratio'] = part['size'] / mmc['size']
+
+    return mmc
+
+
+def get_partition_info(path):
+    part = {}
+
+    filename = os.join(path, 'partition')
+    part['partition'] = int(open(filename).readline())
+
+    # units of start are 512 byte blocks
+    filename = os.join(path, 'start')
+    part['start'] = int(open(filename).readline()) * 512
+
+    # units of size are 512 byte blocks
+    filename = os.join(path, 'size')
+    part['size'] = int(open(filename).readline()) * 512
+
+    return part
+
+
 if __name__ == '__main__':
     from pprint import pprint
     pprint(uptime())
     pprint(stat())
     pprint(meminfo())
+
+    pprint(get_mmc_info('/sys/block/mmcblk0'))
+    pprint(get_mmc_info('/sys/block/mmcblk1'))
