b400a301569a5dd446397375466e1191f91995a3
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-04-22 15:45:55 -0500

    Convert units in meminfo.

--- server/app.py
+++ server/app.py
@@ -15,12 +15,33 @@ def cpustats():
     return jsonify({})
 
 
+memunits = {
+    'kB': 1024,
+}
+
+
 @app.route('/mem')
 def memstats():
     stats = {}
 
     for line in open('/proc/meminfo'):
         fields = line.split(':')
-        stats[fields[0].strip()] = fields[1].strip()
+        key = fields[0].strip()
+        rhs = fields[1].split()
+
+        if len(rhs) == 1:
+            value = int(rhs[0])
+        elif len(rhs) == 2:
+            try:
+                value = int(rhs[0]) * memunits[rhs[1]]
+            except KeyError:
+                value = -1
+
+        stats[key] = value
 
     return jsonify(stats)
+
+
+@app.route('/net')
+def netstats():
+    return jsonify({})
